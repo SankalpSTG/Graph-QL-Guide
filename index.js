c22b14dea8b34c6abf08432a4bf93b57ea06ed5b
sankalppolk123 <sankalppolk123@gmail.com>
2023-03-20 03:12:21 +0530

    Updated App

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 const { ApolloServer, gql } = require("apollo-server")
-
+const productsJson = require("./products.json")
 const typeDefs = gql`
     type Query {
         name: String,
         age: Int,
-        isMarried: Boolean
+        isMarried: Boolean,
+        friends: [String],
+        exGirlfriends: [String],
+        products: [Product]
+    }
+    type Product {
+        name: String,
+        type: String,
+        cost: Int,
+        quantity_available: Int,
+        description: String
     }
 `
 
 const resolvers = {
     Query: {
         name: () => {
-            return "World!"
+            return "Sankalp Pol"
         },
         age: () => {
             return 24
         },
         isMarried: () => {
-            return true
+            return false
+        },
+        friends: () => {
+            return [
+                "Rahul",
+                "Rajesh",
+                "Rohan",
+                "Raj"
+            ]
+        },
+        products: () => {
+            return productsJson
         }
-    }
+    },
 }
 
 const server = new ApolloServer({
